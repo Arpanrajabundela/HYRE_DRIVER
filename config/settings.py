4674deb595df0aa00dfc34d055737f6af80f22be
RamRaghuwanshi <72676225+Arpanrajabundela@users.noreply.github.com>
2026-09-02 12:42:32 +0530

    Update settings.py to use environment variables

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,18 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
